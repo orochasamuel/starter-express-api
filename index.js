441afa32c32dbe71e7471d3ef8cf0371be8ea61f
O Samuel Rocha <samuelro.net@gmail.com>
2023-10-27 11:41:38 -0300

    Update index.js

--- index.js
+++ index.js
@@ -7,4 +7,14 @@ app.all('/', (req, res) => {
 app.get('/teste', (req, res) => {
     res.send('O teste deu certo!')
 })
+app.get('/api/products', (req, res) => {
+let products = [{name: 'Tequila', category: 'drink'}, 
+{name: 'Beer', category: 'drink'}, 
+{name: 'Burger', category: 'food'},
+{name: 'Shawarma', category: 'food'}, 
+{name: 'Wine', category: 'drink'},
+{name: 'Gelatto', category: 'dessert'}];
+    
+    res.send(products)
+})
 app.listen(process.env.PORT || 3000)
